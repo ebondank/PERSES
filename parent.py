c20a10360ae77b1e75cfbe591d052dee95d08d46
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-11 02:20:38 -0700

    Fixing pbV vs. pbR

--- parent.py
+++ parent.py
@@ -4,7 +4,8 @@ import numpy as np
 from epanet import epanet
 import config
 
-# TODO qwd
+# TODO Need to set env variables for server
+
 
 pvcPipesFile = open('D:\\Austin_Michne\\1_11_17\\pvcPipes.txt', 'r')
 pvcPipesList = pvcPipesFile.read().expandtabs().splitlines()
@@ -18,42 +19,42 @@ ironPipesFile = open('D:\\Austin_Michne\\1_11_17\\ironPipes.txt', 'r')
 ironPipesList = ironPipesFile.read().expandtabs().splitlines()
 ironPipesFile.close()
 
-config.ironPipeAges['real'] = np.random.uniform(0, 85, len(ironPipesList))
-config.ironPipeAges['noTemp'] = config.ironPipeAges['real']
-config.ironPipeAges['noTime'] = config.ironPipeAges['real']
+config.ironPipeAges['real'] = list(np.random.uniform(0, 85, len(ironPipesList)))
+config.ironPipeAges['noTemp'] = list(config.ironPipeAges['real'])
+config.ironPipeAges['noTime'] = list(config.ironPipeAges['real'])
 
 pvcCount = len(config.pvcPipesList)
 config.pvcPipeAges['real'] = np.append(np.random.normal(40, 9, pvcCount - 20), np.random.normal(13, 3, 20))
-config.pvcPipeAges['noTemp'] = config.pvcPipeAges['real']
-config.pvcPipeAges['noTime'] = config.pvcPipeAges['real']
+config.pvcPipeAges['noTemp'] = list(config.pvcPipeAges['real'])
+config.pvcPipeAges['noTime'] = list(config.pvcPipeAges['real'])
 
 config.pumpAgeList['real'] = np.array([10, 25])
-config.pumpAgeList['noTemp'] = config.pumpAgeList['real']
-config.pumpAgeList['noTime'] = config.pumpAgeList['real']
+config.pumpAgeList['noTemp'] = list(config.pumpAgeList['real'])
+config.pumpAgeList['noTime'] = list(config.pumpAgeList['real'])
 
 for index, item in enumerate(config.pvcPipesList):
     config.pvcFailureStatus['real'].append(0)
     config.pvcPipeThresholdList['real'].append(np.random.uniform(0, 1, 1)[0])
-config.pvcFailureStatus['noTemp'] = config.pvcFailureStatus['real']
-config.pvcFailureStatus['noTime'] = config.pvcFailureStatus['real']
-config.pvcPipeThresholdList['noTemp'] = config.pvcPipeThresholdList['real']
-config.pvcPipeThresholdList['noTime'] = config.pvcPipeThresholdList['real']
+config.pvcFailureStatus['noTemp'] = list(config.pvcFailureStatus['real'])
+config.pvcFailureStatus['noTime'] = list(config.pvcFailureStatus['real'])
+config.pvcPipeThresholdList['noTemp'] = list(config.pvcPipeThresholdList['real'])
+config.pvcPipeThresholdList['noTime'] = list(config.pvcPipeThresholdList['real'])
 
 for index, item in enumerate(config.ironPipesList):
     config.ironFailureStatus.append(0)
     config.ironPipeThresholdList.append(np.random.uniform(0, 1, 1)[0])
-config.ironFailureStatus['noTemp'] = config.ironFailureStatus['real']
-config.ironFailureStatus['noTime'] = config.ironFailureStatus['real']
-config.ironPipeThresholdList['noTemp'] = config.ironPipeThresholdList['real']
-config.ironPipeThresholdList['noTime'] = config.ironPipeThresholdList['real']
+config.ironFailureStatus['noTemp'] = list(config.ironFailureStatus['real'])
+config.ironFailureStatus['noTime'] = list(config.ironFailureStatus['real'])
+config.ironPipeThresholdList['noTemp'] = list(config.ironPipeThresholdList['real'])
+config.ironPipeThresholdList['noTime'] = list(config.ironPipeThresholdList['real'])
 
 for index, item in enumerate(config.pumpAgeList):
     config.pumpFailureStatusFile.append(0)
     config.pumpThresholdFile.append(np.random.uniform(0, 1, 1)[0])
-config.pumpFailureStatus['noTemp'] = config.pumpFailureStatus['real']
-config.pumpFailureStatus['noTime'] = config.pumpFailureStatus['real']
-config.pumpThresholdList['noTemp'] = config.pumpThresholdList['real']
-config.pumpThresholdList['noTime'] = config.pumpThresholdList['real']
+config.pumpFailureStatus['noTemp'] = list(config.pumpFailureStatus['real'])
+config.pumpFailureStatus['noTime'] = list(config.pumpFailureStatus['real'])
+config.pumpThresholdList['noTemp'] = list(config.pumpThresholdList['real'])
+config.pumpThresholdList['noTime'] = list(config.pumpThresholdList['real'])
 
 # Creating all three databases
 databaseObjectReal = sql.connect(('D:\\Austin_Michne\\tripleSim\\realistic{}.db').format(os.environ['SIMCOUNT']))
