bf9c82e9ced3d1d28ac022a26348dbe68fd6253d
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-19 06:58:54 -0700

    xxx

--- config_c.py
+++ config_c.py
@@ -68,7 +68,7 @@ while (linkCounter < linkList.contents.value):
 
         data['real']['pvc']['age'].append(randpvcAge)
         data['noTemp']['pvc']['age'].append(randpvcAge)
-        data['noTime']['pvc']['age'].append(65)
+        data['noTime']['pvc']['age'].append([65])
 
         data['real']['pvc']['fS'].append(0)
         data['noTemp']['pvc']['fS'].append(0)
