f3cd10b248f4e3911eed3a3fd59e6fb54ad5471a
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-19 06:10:26 -0700

    zzz

--- epanet_c.py
+++ epanet_c.py
@@ -2,7 +2,7 @@ import math
 from config_c import *
 import numpy as np
 import ctypes as ct
-from parent_c import *
+import parent_c
 
 def epanet(batch, simType, dbCursor, dbObject):
     epaCount = 0
@@ -18,13 +18,13 @@ def epanet(batch, simType, dbCursor, dbObject):
 
                 if (int(data[simType]['pvc']['fS'][index + 1]) <= 0):
                     # pipe enable
-                    epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(1))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(1))
                     # no-time simulation config stuff
                     if (simType != 'noTime'):
                         pvcPipeAges[simType][index + 1] = 0
                 # Pipe disable mid run
                 else:
-                    epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
             else:
                 indexSelect = 0
                 indexSelect = (math.trunc(tasMaxACT) - 19)
@@ -35,7 +35,7 @@ def epanet(batch, simType, dbCursor, dbObject):
                 if (float(pvcWeibullList[indexSelect]) > float(data[simType]['pvc']['tH'][index + 1])):
                     data[simType]['pvc']['age'][index + 1] = 0
                     data[simType]['pvc']['tH'][index + 1] = float((np.random.uniform(0, 1, 1))[0])
-                    epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['pvc']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
                     pipeFailureFile = open(('{}_pvcPipeFail.txt').format(simType), 'a')
                     pipeFailureFile.write('%s %s\n' % (index, biHour))
                     pipeFailureFile.close()
@@ -49,9 +49,9 @@ def epanet(batch, simType, dbCursor, dbObject):
             if (int(data[simType]['iron']['fS'][index + 1]) != 0):
                 data[simType]['iron']['fS'][index + 1] = int(data[simType]['iron']['fS'][index + 1]) - 1
                 if (int(data[simType]['iron']['fS'][index + 1]) <= 0):
-                    epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(1))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(1))
                 else:
-                    epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
                 if (simType != 'noTime'):
                     data[simType]['iron']['age'][index + 1] = 0
 
@@ -66,7 +66,7 @@ def epanet(batch, simType, dbCursor, dbObject):
                     if (simType != 'noTime'):
                         data[simType]['iron']['age'][index + 1] = 0
                     data[simType]['iron']['tH'][index + 1] = float((np.random.uniform(0, 1, 1))[0])
-                    epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['iron']['index'][index + 1], ct.c_int(11), ct.c_float(0.0))
                     # Writing to the seperate failure statistics file
                     pipeFailureFile = open(('{}_ironPipeFail.txt').format(simType), 'a')
                     pipeFailureFile.write('%s %s\n' % (index, biHour))
@@ -79,9 +79,9 @@ def epanet(batch, simType, dbCursor, dbObject):
         for index, item in enumerate(pumpList):
             if (int(data[simType]['pump']['fS'][index + 1]) != 0):
                 data[simType]['pump']['fS'][index + 1] = int(data[simType]['pump']['fS'][index + 1]) - 1
-                epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(0.0))
+                parent_c.epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(0.0))
                 if (int(data[simType]['pump']['fS'][index + 1]) <= 0):
-                    epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(1.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(1.0))
                     if (simType != 'noTime'):
                         data[simType]['pump']['age'][index + 1] = 0
 
@@ -97,7 +97,7 @@ def epanet(batch, simType, dbCursor, dbObject):
                     pumpFailureFile = open(('{}_pumpFail.txt').format(simType), 'a')
                     pumpFailureFile.write('%s %s\n' % (index, biHour))
                     pumpFailureFile.close()
-                    epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(0.0))
+                    parent_c.epalib.ENsetlinkvalue(data[simType]['pump']['index'][index + 1], ct.c_int(12), ct.c_float(0.0))
                     # This is based off of the 16 hr repair time, can be
                     # changed to w/e
                     pumpFailureStatus[simType][index + 1] = 8
@@ -105,26 +105,26 @@ def epanet(batch, simType, dbCursor, dbObject):
                     data[simType]['pump']['age'][index + 1] = float(data[simType]['pump']['age'][index + 1]) + biHourToYear
 
         # Does the hydraulic solving
-        errorcode = parent_c.epalib.ENrunH(time)
+        errorcode = parent_c.parent_c.epalib.ENrunH(time)
         print('errorcode: %s' % errorcode)
 
         intCount = ct.c_int(1)
         while (intCount.value < nodeCount.contents.value):
-            epalib.ENgetnodevalue(intCount, ct.c_int(11), nodeValue)
-            epalib.ENgetnodeid(intCount, nodeID)
+            parent_c.epalib.ENgetnodevalue(intCount, ct.c_int(11), nodeValue)
+            parent_c.epalib.ENgetnodeid(intCount, nodeID)
             dbCursor.execute('''INSERT INTO NodeData VALUES (?, ?, ?)''', (biHour, nodeID.value, nodeValue.contents.value))
             intCount.value = intCount.value + 1
 
         dbObject.commit()
         if (parent_c.time.contents.value == 864000):
             parent_c.time.contents = ct.c_int(0)
-        parent_c.epalib.ENnextH(parent_c.timestep)
+        parent_c.parent_c.epalib.ENnextH(parent_c.timestep)
 
         # Saves the hydraulic results file
-        # epalib.ENsaveH()
+        # parent_c.epalib.ENsaveH()
         # Reports the data from the previous run
-        # epalib.ENreport()
-        # epalib.ENclose()
+        # parent_c.epalib.ENreport()
+        # parent_c.epalib.ENclose()
 
         # Closes all of the files open during the simulation
         f.close()
