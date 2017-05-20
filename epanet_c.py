a5bb09fff3a11730e5126c2599a8c04027ee3072
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-20 14:55:20 -0700

    zzz

--- epanet_c.py
+++ epanet_c.py
@@ -78,7 +78,7 @@ def epanet(batch, simType, dbCursor, dbObject):
                 if (simType != 'noTime'):
                     data[simType]['iron']['age'][index] = float(data[simType]['iron']['age'][index]) + biHourToYear
         for index, item in enumerate(data[simType]['pump']['index']):
-            config_c.printItem = data[simType]['pump']['fS'][index]
+            printItem = data[simType]['pump']['fS'][index]
             if data[simType]['pump']['fS'][index] != 1000:
                 data[simType]['pump']['fS'][index] = int(data[simType]['pump']['fS'][index]) - 1
                 epalib.ENsetlinkvalue(data[simType]['pump']['index'][index], ct.c_int(12), ct.c_float(0.0))
