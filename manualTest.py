7a129bd78a6679ebd14e1cf279769f0eb287efd1
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-05-26 19:31:49 -0700

    zzz

--- manualTest.py
+++ manualTest.py
@@ -63,10 +63,10 @@ if errorcode != 0:
 print('\n---------------\n')
 
 i = 0
-ID = 15
+ID = 335
 time = ct.pointer(ct.c_long(1))
 timestep = ct.pointer(ct.c_long(1))
-epalib.ENsetlinkvalue(ct.c_int(335), ct.c_int(12), ct.c_float(0.0))
+epalib.ENsetlinkvalue(ct.c_char_p(str(ID).encode('utf-8')), ct.c_int(12), ct.c_float(0.0))
 while True:
     i += 1
     print('iter', i)
