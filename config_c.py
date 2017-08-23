f73831f0c29cfdef1d833cb3d9fb4f97af86a16b
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-08-23 15:15:25 -0700

    testing temp curves

--- config_c.py
+++ config_c.py
@@ -8,7 +8,7 @@ data = {'real':{'iron':{'ctH':list(), 'ltH': list(), 'age':list(), 'fS':list(), 
 
 normal_run_list = [list()]*24
 
-tasFile = open('tasMaxBD85.txt', 'r')
+tasFile = open('tasMaxBD.txt', 'r')
 tasList = tasFile.read().expandtabs().splitlines()
 tasFile.close()
 
