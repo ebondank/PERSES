b09d3a15ce4dadbbffcd104d94e9110de205edde
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-10-17 02:02:50 -0700

    Working with synced probabilities

--- monte_carlo.py
+++ monte_carlo.py
@@ -11,7 +11,7 @@ class component_populations(object):
         self.db_cur = db_cur
         self.table_name = table_name
         self.god_factor_list = component_god_factor_list
-        self.god_factor_count = 0
+        self.god_factor_count = [0] * len(self.god_factor_list)
         self.biHourToYear = .0002283105022831050228310502283105
         f_ = open(temp_curve_file, 'r')
         f_list = f_.read().expandtabs().splitlines()
@@ -43,26 +43,22 @@ class component_populations(object):
             else:
                 new_list.append(item)
         self.distribution_list = new_list
-        self.god_factor_list_current = list()
         self.exposure_array = list()
         count = 0
         while (count < amount):
-            self.god_factor_list_current.append(self.god_factor_list[0][self.god_factor_count])
             self.exposure_array.append(0)
             count += 1
     
 
     def failure_evaluation(self, index, time, component_type):
-        
         per_failed1 = self.distribution_list[math.floor(float(self.exposure_array[index]))]
         per_failed2 = self.distribution_list[math.ceil(float(self.exposure_array[index]))]
         per_failed = (float(per_failed2) - float(per_failed1)) * (float(self.exposure_array[index]) - math.floor(float(self.exposure_array[index]))) + float(per_failed1)
 
-        if (per_failed > self.god_factor_list_current[index]):
+        if (per_failed > self.god_factor_list[index][self.god_factor_count[index]]):
             self.db_cur.execute(('''INSERT INTO {} VALUES (?, ?, ?)''').format(self.table_name), (time, index, component_type))
             self.exposure_array[index] = 0
-            self.god_factor_count += 1
-            self.god_factor_list_current[index] = self.god_factor_list[self.god_factor_count]
+            self.god_factor_count[index] += 1
         else:
             self.exposure_array[index] = self.exposure_array[index] + (self.biHourToYear * float(self.temp_curve[math.floor(time/12)]))
             
@@ -81,21 +77,22 @@ if __name__ == "__main__":
         db_cur.execute(('''CREATE TABLE {} (Bihour_Count real, NodeID real, componentType real)''').format(item))
     
     pop_list = ["pump", "pvc", "iron"]
-    god_factor_simulation_syncing = {"pump": [], "pvc": [], "iron": []}
+    god_factor_simulation_syncing = [list(), list(), list()]
     component_count = 250
     count = 0
 
     # {"pump": [1, .2, .6], etc.}
     while (count < component_count):
-        for item in pop_list:
-            god_factor_simulation_syncing[item].append(list(np.random.uniform(0, 1, 1000)))
+        random_list = list(np.random.uniform(0, 1, 1000))
+        for index, item in enumerate(pop_list):
+            god_factor_simulation_syncing[index].append(random_list)
         count += 1
     for simulation in db_table_list:
         statistics_dict = dict()
         for index, item in enumerate(pop_list):
             temp_curve = ("{}.txt").format(simulation)
             cdf_curve = ("{}_made_cdf.txt").format(item)
-            gf = god_factor_simulation_syncing[item]
+            gf = god_factor_simulation_syncing[index]
             new_simulation = component_populations(temp_curve, cdf_curve, item, gf, component_count, db_cur, simulation)
             statistics_dict[("{}_{}").format(simulation, item)] = new_simulation
             print(item)
@@ -103,10 +100,11 @@ if __name__ == "__main__":
         goal_time = 350000
         while time < goal_time:
             for population in statistics_dict.keys():
-                testing_tuple_bug = enumerate(statistics_dict[population].god_factor_list)
-                for index, value in testing_tuple_bug:
+                index = 0
+                while index < component_count:
                     component_type = list(population.split('_'))[1]
                     statistics_dict[population].failure_evaluation(index, time, component_type)
+                    index += 1
             time += 1
     db_obj.commit()
     db_obj.close()
