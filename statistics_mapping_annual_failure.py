21067228542d523e60d7f99badb78663de5f3438
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-10-23 00:25:21 -0700

    Simplifying

--- statistics_mapping_annual_failure.py
+++ statistics_mapping_annual_failure.py
@@ -4,7 +4,7 @@ import sqlite3 as sql
 
 # file1 = open(os.path.expanduser('8-08-17/noTime_yesCC_ironPipeFail.txt'), 'r')
 # list1 = file1.read().splitlines()
-Path = '10-23-17/statistics.db'
+Path = 'statistics.db'
 run_time = 80
 db = sql.connect(Path)
 com = db.cursor()
